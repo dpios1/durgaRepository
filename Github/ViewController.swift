3122a6f373f77254036ea0cf75adf3b6a48cd2fd
welcome <welcome@welcomes-mac.local>
2016-11-01 21:22:42 +0530

    Printed the button

--- Github/ViewController.swift
+++ Github/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onClick(sender: AnyObject) {
+        print("Commited eedited")
+    }
 
 }
 
